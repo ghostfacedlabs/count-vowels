70c7023fbbeb5fd0b4a5fd042222647d080b5543
ghostfacedlabs <ghostfacedlabs@gmail.com>
2021-07-22 08:07:53 -0700

    v1.0 application complete

--- src/com/ghostfacedlabs/Main.java
+++ src/com/ghostfacedlabs/Main.java
@@ -1,8 +1,48 @@
 package com.ghostfacedlabs;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter some text:");
+        String text = scan.nextLine().toLowerCase();
+        int totalVowels = 0;
+        int countA = 0;
+        int countE = 0;
+        int countI = 0;
+        int countO = 0;
+        int countU = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            char letter = text.charAt(i);
+
+            if (letter == 'a') {
+                totalVowels++;
+                countA++;
+            }
+            if (letter == 'e') {
+                totalVowels++;
+                countE++;
+            }
+            if (letter == 'i') {
+                totalVowels++;
+                countI++;
+            }
+            if (letter == 'o') {
+                totalVowels++;
+                countO++;
+            }
+            if (letter == 'u') {
+                totalVowels++;
+                countU++;
+            }
+
+
+        }
+
+        System.out.println("There are " + totalVowels + " vowels in the text");
+        System.out.println("A: " + countA + ", E: " + countE + ", I: " + countI + ", O: " + countO + ", U: " + countU);
     }
 }
